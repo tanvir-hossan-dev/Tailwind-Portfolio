2e48e19a82862c3e7f3fd0095d83d7809aff2759
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-09-19 23:55:55 +0600

    portfolio folersetup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        para: "#6C6C6C",
+        primary: "#F40404",
+        yellowBorder: "#FFB800",
+      },
+    },
+  },
+  plugins: [],
+};
